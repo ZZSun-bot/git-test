f221d49ddfb1fb1a35bf2a2f9ee668215dc8fbca
Zhengzhong Sun <s192218@student.dtu.dk>
2022-06-06 15:52:10 +0200

    master fix

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
             System.out.println("hello git");
             System.out.println("hello git1");
             System.out.println("hello git2");
+            System.out.println("Master fix");
     }
 }
